f70f88f64874758b15b018a596a9a2693c587785
Yuto Matake <mata0100@algonquinlive.com>
2024-09-09 14:15:12 -0400

    laptop からのコミット２

--- MatakeTest/src/First.java
+++ MatakeTest/src/First.java
@@ -4,6 +4,7 @@ public class First {
 	public static void main(String[] args) {
 		System.out.print("hello");
 		System.out.print("Hello from laptop");
+		System.out.print("Hello from laptop2");
 
 	}
 
